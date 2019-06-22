40aed25b97b4e227ef53f5d9d9b8136de707b725
ericheredia <ericdavidheredia@gmail.com>
2019-06-22 16:12:30 -0500

    Updates

---
+++ precache-manifest.9cc951fe49365fa9e32e95942a9c9f5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "afdaafd4c60c0b128f24",
+    "url": "/blockstack-been-there/static/css/main.7ca66e89.chunk.css"
+  },
+  {
+    "revision": "afdaafd4c60c0b128f24",
+    "url": "/blockstack-been-there/static/js/main.1ebc7a58.chunk.js"
+  },
+  {
+    "revision": "c59d0eb567c64c4d0398",
+    "url": "/blockstack-been-there/static/js/runtime~main.11875696.js"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/blockstack-been-there/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "4280ff19c308ff672320e1cb16fb3c75",
+    "url": "/blockstack-been-there/index.html"
+  }
+];
